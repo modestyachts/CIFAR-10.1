737940f795a7a0ba51cdd00d313757bd048c2972
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2018-06-26 17:32:33 -0700

    work on CIFAR-10 loading functions in utils.py

--- code/utils.py
+++ code/utils.py
@@ -76,13 +76,10 @@ def load_v6_distances_to_cifar10(
     return result
 
 
-def load_cifar10_by_keyword():
-    '''Returns a dictionary maping each keyword in CIFAR10 to a list of
-       TinyImage indices.'''
-    other_data_path = os.path.join(os.path.dirname(__file__), '../other_data/')
-    keywords_filepath = os.path.join(other_data_path, 'cifar10_keywords.json')
-    with open(keywords_filepath) as f:
-        cifar10_keywords = json.load(f)
+def load_cifar10_by_keyword(unique_keywords=True, version_string=''):
+    cifar10_keywords = load_cifar10_keywords(unique_keywords=unique_keywords,
+                                             lists_for_unique=True,
+                                             version_string=version_string)
     cifar10_by_keyword = {}
     for ii, keyword_entries in enumerate(cifar10_keywords):
         for entry in keyword_entries:
@@ -91,6 +88,30 @@ def load_cifar10_by_keyword():
                 cifar10_by_keyword[cur_keyword] = []
             cifar10_by_keyword[cur_keyword].append(ii)
     return cifar10_by_keyword
+
+
+def load_cifar10_keywords(unique_keywords=True, lists_for_unique=False, version_string=''):
+    other_data_path = os.path.join(os.path.dirname(__file__), '../other_data/')
+    filename = 'cifar10_keywords'
+    if unique_keywords:
+        filename += '_unique'
+    if version_string:
+        filename += '_' + version_string
+    filename += '.json'
+    keywords_filepath = os.path.abspath(os.path.join(other_data_path, filename))
+    print('Loading keywords from file {}'.format(keywords_filepath))
+    assert pathlib.Path(keywords_filepath).is_file()
+    with open(keywords_filepath) as f:
+        cifar10_keywords = json.load(f)
+    if unique_keywords and lists_for_unique:
+        result = []
+        for entry in cifar10_keywords:
+            result.append([entry])
+    else:
+        result = cifar10_keywords
+    assert len(result) == 60000
+    return result
+
 
 def compute_accuracy(pred, labels):
     return np.sum(pred == labels) / float(len(labels))
