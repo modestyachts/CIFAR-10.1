0de05e80944e329f0f3b1398d5805635c483a371
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2018-06-30 19:06:29 -0700

    work on the dataset loading code in utils

--- code/utils.py
+++ code/utils.py
@@ -22,18 +22,24 @@ def np_to_png(a, fmt='png', scale=1):
     return f.getvalue()
 
 
-def load_new_test_data(version='default'):
+def load_new_test_data(version_string='', load_tinyimage_indices=False):
     data_path = os.path.join(os.path.dirname(__file__), '../datasets/')
     filename = 'cifar10.1'
-    if version in ['v4', 'v6']:
-        filename += '_' + version
+    if version_string == '':
+        version_string = 'v7'
+    if version_string in ['v4', 'v6', 'v7']:
+        filename += '_' + version_string
     else:
-        raise ValueError('Unknown dataset version "{}".'.format(version))
+        raise ValueError('Unknown dataset version "{}".'.format(version_string))
     label_filename = filename + '_labels.npy'
     imagedata_filename = filename + '_data.npy'
-    label_filepath = os.path.join(data_path, label_filename)
-    imagedata_filepath = os.path.join(data_path, imagedata_filename)
+    label_filepath = os.path.abspath(os.path.join(data_path, label_filename))
+    imagedata_filepath = os.path.abspath(os.path.join(data_path, imagedata_filename))
+    print('Loading labels from file {}'.format(label_filepath))
+    assert pathlib.Path(label_filepath).is_file()
     labels = np.load(label_filepath)
+    print('Loading image data from file {}'.format(imagedata_filepath))
+    assert pathlib.Path(imagedata_filepath).is_file()
     imagedata = np.load(imagedata_filepath)
     assert len(labels.shape) == 1
     assert len(imagedata.shape) == 4
@@ -41,11 +47,24 @@ def load_new_test_data(version='default'):
     assert imagedata.shape[1] == 32
     assert imagedata.shape[2] == 32
     assert imagedata.shape[3] == 3
-    if version == 'v6':
+    if version_string == 'v6' or version_string == 'v7':
         assert labels.shape[0] == 2000
-    elif version == 'v4':
+    elif version_string == 'v4':
         assert labels.shape[0] == 2021
-    return imagedata, labels
+
+    if not load_tinyimage_indices:
+        return imagedata, labels
+    else:
+        ti_indices_data_path = os.path.join(os.path.dirname(__file__), '../other_data/')
+        ti_indices_filename = 'cifar10.1_' + version_string + '_ti_indices.json'
+        ti_indices_filepath = os.path.abspath(os.path.join(ti_indices_data_path, ti_indices_filename))
+        print('Loading Tiny Image indices from file {}'.format(ti_indices_filepath))
+        assert pathlib.Path(ti_indices_filepath).is_file()
+        with open(ti_indices_filepath, 'r') as f:
+            tinyimage_indices = json.load(f)
+        assert type(tinyimage_indices) is list
+        assert len(tinyimage_indices) == labels.shape[0]
+        return imagedata, labels, tinyimage_indices
 
 
 def load_distances_to_cifar10(version_string=''):
