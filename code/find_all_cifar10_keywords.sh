2b1259a6c778c5771e95b7b706b0e811951987cd
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2018-07-02 06:16:29 +0000

    Adding keyword unifying step to bash script

--- code/find_all_cifar10_keywords.sh
+++ code/find_all_cifar10_keywords.sh
@@ -10,3 +10,5 @@ python3 find_cifar10_keywords.py 60000000 10000000
 python3 find_cifar10_keywords.py 70000000 9302017
 
 python3 unify_cifar10_keywords.py
+
+python3 make_cifar10_keywords_unique.py
